a6253b8a34734db7a18b8bb4f8d61c0b65d5d929
alexandronischenko <alexandr.onischenko.2003@gmail.com>
2024-03-30 23:01:52 +0300

    Added changes

--- Services/Extensions/UITableViewCell+.swift
+++ Services/Extensions/UITableViewCell+.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import UIKit
+
+extension UITableViewCell {
+    func configureView(service: Service) {
+        accessoryType = .disclosureIndicator
+        let url = URL(string: service.iconURL)
+        let imageView = UIImageView()
+        imageView.kf.setImage(
+            with: url,
+            placeholder: UIImage(named: "photo")
+        )
+
+        var content = UIListContentConfiguration.sidebarCell()
+        content.imageProperties.maximumSize = CGSize(width: 64, height: 64)
+        content.imageToTextPadding = 16
+        content.imageProperties.cornerRadius = 4
+        content.secondaryTextProperties.font = .systemFont(ofSize: 16, weight: .light)
+
+        content.image = imageView.image
+        content.secondaryText = service.description
+        content.secondaryTextProperties.numberOfLines = 0
+
+        content.text = "\(service.name)"
+        contentConfiguration = content
+    }
+}
--- Services/Repository/ServiceRepository.swift
+++ Services/Repository/ServiceRepository.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+class ServiceRepository {
+    static var shared = ServiceRepository()
+
+    private var path = "https://publicstorage.hb.bizmrg.com/sirius/result.json"
+
+    private init() {}
+
+    enum ErrorResponse: Error {
+        case failedToGetData
+        case failedToDecode
+    }
+
+    func getServices(completion: @escaping ((Result<[Service], Error>) -> ())) {
+        guard let url = URL(string: path) else {
+            completion(.failure(ErrorResponse.failedToGetData))
+            return
+        }
+        let request = URLRequest(url: url)
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data else {
+                completion(.failure(ErrorResponse.failedToGetData))
+                return
+            }
+            let decoder = JSONDecoder()
+            guard let message = try? decoder.decode(ServiceMessage.self, from: data) else {
+                completion(.failure(ErrorResponse.failedToDecode))
+                return
+            }
+            completion(.success(message.body.services))
+        }.resume()
+    }
+}
